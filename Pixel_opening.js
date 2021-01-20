9ece73dabe2eab9a461e19e8097b12c60927819f
skhoosh <49808635+skhoosh@users.noreply.github.com>
2021-01-20 15:58:07 +0800

    first commit

---
+++ Pixel_opening.js
@@ -0,0 +1,679 @@
+var pixelOpening = {
+	"type" : "FeatureCollection",
+	"name" : "opening_shp",
+	"features" : [
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7897537775, 1.3009440814 ],
+					[ 103.7898088962, 1.300884906 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.789749953, 1.3010256635 ],
+					[ 103.7897670071, 1.3010202501 ],
+					[ 103.7897670071, 1.3010202501 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896836059, 1.3008475098 ],
+					[ 103.7896895652, 1.3008661248 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896960659, 1.3008207095 ],
+					[ 103.789705776, 1.3008178009 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896922268, 1.3007811398 ],
+					[ 103.7897033829, 1.3007773927 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.789621981, 1.3007827061 ],
+					[ 103.7896200237, 1.3007756054 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896341425, 1.3007919038 ],
+					[ 103.7896317465, 1.3007834854 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7894956564, 1.3008402997 ],
+					[ 103.7895031732, 1.3008377248 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7895346774, 1.3009696127 ],
+					[ 103.7895376938, 1.3009789722 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896100605, 1.3011546745 ],
+					[ 103.7896210945, 1.3011511225 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7897110414, 1.3012066424 ],
+					[ 103.7897190873, 1.3012042102 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7897021019, 1.3012093448 ],
+					[ 103.7897069758, 1.3012078715 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7897218052, 1.3012033886 ],
+					[ 103.789737685, 1.3011985882 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7897649623, 1.3011903424 ],
+					[ 103.789781139, 1.3011854523 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7897978727, 1.3011803937 ],
+					[ 103.7898214036, 1.3011732805 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7898091016, 1.301117908 ],
+					[ 103.7898054912, 1.301106564 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7898129648, 1.3008083871 ],
+					[ 103.7897813417, 1.30079088 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.789680782, 1.3007121809 ],
+					[ 103.7896887405, 1.3007097637 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.789933063, 1.3007084589 ],
+					[ 103.7899294862, 1.3006972634 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7899592244, 1.3007006482 ],
+					[ 103.7899557875, 1.3006900109 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7899735792, 1.3006856125 ],
+					[ 103.789987083, 1.3006811869 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7894945156, 1.3008449962 ],
+					[ 103.7894972369, 1.3008534399 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7894858489, 1.3008181044 ],
+					[ 103.789488731, 1.3008270473 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896922268, 1.3007811398 ],
+					[ 103.7896844016, 1.3007834686 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896212736, 1.3011517368 ],
+					[ 103.789610889, 1.3011543548 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7897626519, 1.3010040932 ],
+					[ 103.7897465288, 1.3010090757 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896921293, 1.3008803467 ],
+					[ 103.7896869468, 1.3008644314 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7895036969, 1.3008370852 ],
+					[ 103.7894928989, 1.3008399795 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896442279, 1.3007947688 ],
+					[ 103.7896376185, 1.300796693 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896790118, 1.3007842397 ],
+					[ 103.7896712244, 1.3007867024 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896930523, 1.300780879 ],
+					[ 103.7896852843, 1.3007826422 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896930523, 1.300780879 ],
+					[ 103.7897030822, 1.3007776229 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896960399, 1.3008203658 ],
+					[ 103.789705449, 1.3008172511 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7897789817, 1.3008374952 ],
+					[ 103.7897932717, 1.3008461081 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7898740859, 1.300839759 ],
+					[ 103.7898792871, 1.3008551245 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7899377407, 1.3007225769 ],
+					[ 103.7899345806, 1.3007126617 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7899570595, 1.3007013109 ],
+					[ 103.789950024, 1.3007034647 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.789619525, 1.3011519211 ],
+					[ 103.789610924, 1.3011550487 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 3"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7897192808, 1.300872535 ],
+					[ 103.7897035619, 1.3008773925 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 3"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896433461, 1.3008359633 ],
+					[ 103.7896383128, 1.3008203677 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 3"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896954009, 1.3008197539 ],
+					[ 103.7897052244, 1.3008167924 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 3"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896375426, 1.3007968384 ],
+					[ 103.7896443537, 1.3007947003 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 3"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896705541, 1.3007864272 ],
+					[ 103.7896779422, 1.3007840654 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 3"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7896849577, 1.3007819343 ],
+					[ 103.789693417, 1.3007794584 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 3"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7897026499, 1.3007765486 ],
+					[ 103.789693417, 1.3007794584 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 3"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7895030717, 1.3008385837 ],
+					[ 103.7894953452, 1.3008410761 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 3"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.7897272007, 1.3008643419 ],
+					[ 103.7897244179, 1.3008556483 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 3"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "LineString",
+				"coordinates" : [
+					[ 103.789931502, 1.3007048762 ],
+					[ 103.7899286897, 1.300695776 ]
+				]
+			},
+			"properties" : {
+				"id" : 0,
+				"Level" : "Level 3"
+			}
+		}
+	]
+}
---
+++ Pixel_points.js
@@ -0,0 +1,295 @@
+var pixelPoints = {
+	"type" : "FeatureCollection",
+	"name" : "pixel_unit00",
+	"features" : [
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 103.7900124, 1.3007579 ]
+			},
+			"properties" : {
+				"file_name" : "16080183937934051999380676123385.jpg",
+				"name" : "PIXEL",
+				"category" : "ramp",
+				"exif_x" : 103.7900124,
+				"exif_y" : 1.3007579,
+				"user_x" : 103.7900124,
+				"user_y" : 1.3007579,
+				"level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 103.789956, 1.300688 ]
+			},
+			"properties" : {
+				"file_name" : "16080184679546158102498681418263.jpg",
+				"name" : "PIXEL",
+				"category" : "door",
+				"exif_x" : 103.7899956,
+				"exif_y" : 1.3007411,
+				"user_x" : 103.789956,
+				"user_y" : 1.300688,
+				"level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 103.789682, 1.300849 ]
+			},
+			"properties" : {
+				"file_name" : "16080185726141261521443873974021.jpg",
+				"name" : "PIXEL IMPIXEL STUDIOS 2",
+				"category" : "door",
+				"exif_x" : 103.790052,
+				"exif_y" : 1.3006268,
+				"user_x" : 103.789682,
+				"user_y" : 1.300849,
+				"level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 103.789757, 1.301021 ]
+			},
+			"properties" : {
+				"file_name" : "16080186826317669740001680756632.jpg",
+				"name" : "IMPIXEL STUDIOS 3",
+				"category" : "door",
+				"exif_x" : 103.7898371,
+				"exif_y" : 1.3009313,
+				"user_x" : 103.789757,
+				"user_y" : 1.301021,
+				"level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 103.789608, 1.300773 ]
+			},
+			"properties" : {
+				"file_name" : "16080188720665175827397175622217.jpg",
+				"name" : "Stair 2",
+				"category" : "stairs",
+				"exif_x" : 103.789685,
+				"exif_y" : 1.3007766,
+				"user_x" : 103.789608,
+				"user_y" : 1.300773,
+				"level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 103.789712, 1.300836 ]
+			},
+			"properties" : {
+				"file_name" : "20201215_160025.jpg",
+				"name" : "ramp",
+				"category" : "ramp",
+				"exif_x" : 103.7899082,
+				"exif_y" : 1.3009447,
+				"user_x" : 103.789712,
+				"user_y" : 1.300836,
+				"level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 103.7898117, 1.3008636 ]
+			},
+			"properties" : {
+				"file_name" : "20201215_155228.jpg",
+				"name" : "ramp",
+				"category" : "ramp",
+				"exif_x" : 103.7898117,
+				"exif_y" : 1.3008636,
+				"user_x" : 103.7898117,
+				"user_y" : 1.3008636,
+				"level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 103.789791, 1.300797 ]
+			},
+			"properties" : {
+				"file_name" : "20201215_155234.jpg",
+				"name" : "IMPIXEL STUDIOS 1",
+				"category" : "door",
+				"exif_x" : 103.7898117,
+				"exif_y" : 1.3008636,
+				"user_x" : 103.789791,
+				"user_y" : 1.300797,
+				"level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 103.789774, 1.300819 ]
+			},
+			"properties" : {
+				"file_name" : "20201215_155243.jpg",
+				"name" : "stairs",
+				"category" : "stairs",
+				"exif_x" : 103.7898117,
+				"exif_y" : 1.3008636,
+				"user_x" : 103.789774,
+				"user_y" : 1.300819,
+				"level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 103.789722, 1.300811 ]
+			},
+			"properties" : {
+				"file_name" : "20201215_155258.jpg",
+				"name" : "Server Room",
+				"category" : "door",
+				"exif_x" : 103.7898117,
+				"exif_y" : 1.3008636,
+				"user_x" : 103.789722,
+				"user_y" : 1.300811,
+				"level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 103.789685, 1.300857 ]
+			},
+			"properties" : {
+				"file_name" : "20201215_155301.jpg",
+				"name" : "IMPIXEL STUDIOS 2",
+				"category" : "door",
+				"exif_x" : 103.7898117,
+				"exif_y" : 1.3008636,
+				"user_x" : 103.789685,
+				"user_y" : 1.300857,
+				"level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 103.789698, 1.300817 ]
+			},
+			"properties" : {
+				"file_name" : "20201215_155305.jpg",
+				"name" : "lift",
+				"category" : "lift",
+				"exif_x" : 103.7898117,
+				"exif_y" : 1.3008636,
+				"user_x" : 103.789698,
+				"user_y" : 1.300817,
+				"level" : "Level 1"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 103.789876, 1.300846 ]
+			},
+			"properties" : {
+				"file_name" : "20201215_155715.jpg",
+				"name" : "TusStar",
+				"category" : "door",
+				"exif_x" : 103.7900172,
+				"exif_y" : 1.300892,
+				"user_x" : 103.789876,
+				"user_y" : 1.300846,
+				"level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 103.789685, 1.300871 ]
+			},
+			"properties" : {
+				"file_name" : "20201215_155742.jpg",
+				"name" : "games@PIXEL Studios",
+				"category" : "door",
+				"exif_x" : 103.7898557,
+				"exif_y" : 1.3009451,
+				"user_x" : 103.789685,
+				"user_y" : 1.300871,
+				"level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 103.789699, 1.300816 ]
+			},
+			"properties" : {
+				"file_name" : "20201215_155745.jpg",
+				"name" : "lift",
+				"category" : "lift",
+				"exif_x" : 103.7898557,
+				"exif_y" : 1.3009451,
+				"user_x" : 103.789699,
+				"user_y" : 1.300816,
+				"level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 103.789752, 1.301008 ]
+			},
+			"properties" : {
+				"file_name" : "20201215_155756.jpg",
+				"name" : "games@PIXEL Studios",
+				"category" : "door",
+				"exif_x" : 103.7898557,
+				"exif_y" : 1.3009451,
+				"user_x" : 103.789752,
+				"user_y" : 1.301008,
+				"level" : "Level 2"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 103.789729, 1.30082 ]
+			},
+			"properties" : {
+				"file_name" : "20201215_155920.jpg",
+				"name" : "ramp",
+				"category" : "ramp",
+				"exif_x" : 103.7898215,
+				"exif_y" : 1.3009339,
+				"user_x" : 103.789729,
+				"user_y" : 1.30082,
+				"level" : "Level 2"
+			}
+		}
+	]
+}
